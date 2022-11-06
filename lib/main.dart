8af3a8a9340dcbf6bbcb50b012a7577cbfb751bf
Saulo Cezar <sauloviva@souunilavras.comexit>
2022-11-06 16:23:15 -0300

    segundo comit

--- lib/main.dart
+++ lib/main.dart
@@ -12,27 +12,27 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Titulo Novo', // título do aplicativo - display no celular
       theme: ThemeData(
-        primarySwatch: Colors.purple, // linha de seleçãoo de cor
+        primarySwatch: Colors.purple, // linha de seleção de cor
       ),
-      home: const MyHomePage(title: 'Pagina Inicial'), // título da pagina
+      home: const MyHomePage(title: 'Pagina Inicial'), // título da pagina - principal widget 
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatefulWidget { // construtor da classe - armazenamento de dados mutáveis  
   const MyHomePage({super.key, required this.title});
 
-  final String title;
+  final String title; // título final recebido pelo valor passado pelo construtor da linha 23
 
-  @override
+  @override // anotação  ou algo está sendo sobrescrito 
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _counter = 0; // linha que recebe um inteiro que se inicia em 0
 
   void _incrementCounter() {
-    // responsavel por contar o numero de vezes do click
+    // responsável por contar o numero de vezes do click
     setState(() {
       _counter++;
     });
@@ -40,16 +40,16 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
+    return Scaffold( // widget que representa uma página
+      appBar: AppBar( // componete superior ou inferior da página do aplicativo - contém barra de ferramentas  
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
+      body: Center( // corpo do texto do aplicativo abaixo do appBar
+        child: Column( // child usado apenas com um widget
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          children: <Widget>[ // children deve ser utilizado como uma lista de widgets
             const Text(
-              'Voce apertou o botao:', // informacao ao usuario de numero de vezes que apertou o botao
+              'Voce apertou o botão:', // informação ao usuario de numero de vezes que apertou o botão
             ),
             Text(
               '$_counter',
@@ -59,10 +59,10 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Aperte aqui', // informacao ao usuario para apertar o botao
+        onPressed: _incrementCounter, // função executada quando o usuário clica no botão
+        tooltip: 'Aperte aqui', // informação ao usúario para apertar o botão ou poderá ser falado para o usuário 
         child: const Icon(Icons.add), // icone para ser clicado pelo usuario
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ), 
     );
   }
 }
